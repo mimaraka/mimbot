d234579d728ca010aa2302b925fe269b76e3ff43
mimaraka <nj.ot.niconico@gmail.com>
2022-08-17 14:01:06 +0900

    bpm analyzer

--- mimbot.py
+++ mimbot.py
@@ -3,8 +3,9 @@ from discord.ext import commands
 #herokuでcv2をimportするとエラーが出たのでとりあえずPillowで代用
 import discord
 from PIL import Image
+import librosa
 import math
-#import numpy as np
+import numpy as np
 import os
 import random
 import requests
@@ -31,7 +32,7 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = '\n'.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
@@ -65,6 +66,54 @@ async def on_message(ctx):
 ##########################################################################
 ####    Bot Command
 ##########################################################################
+
+@bot.command()
+async def bpm(ctx):
+    duration = 30
+    x_sr = 200
+    bpm_min, bpm_max = 60, 240
+
+    if ctx.message.reference is None:
+        await ctx.reply('適用したい音声ファイルが添付されたメッセージに返信してください', mention_author=False)
+        return
+
+    mes = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+
+    if mes.attachments[0] is None:
+        await ctx.reply('返信元のメッセージにファイルが添付されていません', mention_author=False)
+        return
+
+    file_name = mes.attachments[0].filename
+
+    await mes.attachments[0].save(file_name)
+
+    mes_pros = await ctx.reply('処理中です…', mention_author=False)
+
+    # 楽曲の信号を読み込む
+    y, sr = librosa.load(file_name, offset=38, duration=duration, mono=True)
+
+    # ビート検出用信号の生成
+    # リサンプリング & パワー信号の抽出
+    x = np.abs(librosa.resample(y, sr, x_sr)) ** 2
+    x_len = len(x)
+
+    # 各BPMに対応する複素正弦波行列を生成
+    M = np.zeros((bpm_max, x_len), dtype=np.complex)
+    for bpm in range(bpm_min, bpm_max): 
+        thete = 2 * np.pi * (bpm/60) * (np.arange(0, x_len) / x_sr)
+        M[bpm] = np.exp(-1j * thete)
+
+    # 各BPMとのマッチング度合い計算
+    #（複素正弦波行列とビート検出用信号との内積）
+    x_bpm = np.abs(np.dot(M, x))
+
+    # BPM　を算出
+    bpm = np.argmax(x_bpm)
+
+    await mes_pros.delete()
+    await ctx.send(f'BPM: {bpm}')
+    os.remove(file_name)
+
 
 #画像に各種エフェクトをかける
 @bot.command(aliases=['fx', 'effects'])
