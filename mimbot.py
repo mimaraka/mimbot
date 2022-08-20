a57335c8639d4e6c27fb8f087e7b8cf3125daf00
mimaraka <nj.ot.niconico@gmail.com>
2022-08-20 13:05:05 +0900

    debug

--- mimbot.py
+++ mimbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 #import cv2
 #herokuでcv2をimportするとエラーが出たのでとりあえずPillowで代用
 import discord
+import glob
 from PIL import Image
 #import librosa
 import math
@@ -16,6 +17,27 @@ intents = discord.Intents.all()
 
 #Botの接頭辞を"^"にする
 bot = commands.Bot(command_prefix='^', intents=intents, case_insensitive=True)
+
+
+
+##########################################################################
+####    Functions
+##########################################################################
+
+#添付ファイル処理用の関数
+async def attachments_procedure(ctx):
+    #返信をしていなかった場合
+    if ctx.message.reference is None:
+        #直前のメッセージの添付ファイルの取得を試みる
+        
+        await ctx.reply('ファイルが添付されたメッセージに返信してください', mention_author=False)
+        return
+
+    mes = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+
+    if mes.attachments[0] is None:
+        await ctx.reply('返信元のメッセージにファイルが添付されていません', mention_author=False)
+        return
 
 
 
@@ -306,6 +328,168 @@ async def removebg(ctx):
             os.remove('removebg_temp_output.png')
     else:
         await ctx.send(f"Error:{response.status_code} {response.text}")
+
+
+@bot.command()
+async def debug(ctx):
+    await ctx.send(ctx.message.id)
+
+
+# ウマ娘ガチャシミュレーター
+@bot.command()
+async def uma(ctx):
+    uma_gacha_lists = [
+        # [ウマ娘の名称, レア度(ピックアップは+10)]
+        # ガチャ詳細サイト
+        # https://umamusume.cygames.jp/#/gacha
+        ["[スペシャルドリーマー]スペシャルウィーク", 3],
+        ["[サイレントイノセンス]サイレンススズカ", 3],
+        ["[トップ・オブ・ジョイフル]トウカイテイオー", 3],
+        ["[フォーミュラオブルージュ]マルゼンスキー", 3],
+        ["[スターライトビート]オグリキャップ", 3],
+        ["[ワイルド・フロンティア]タイキシャトル", 3],
+        ["[エレガンス・ライン]メジロマックイーン", 3],
+        ["[ロード・オブ・エンペラー]シンボリルドルフ", 3],
+        ["[ローゼスドリーム]ライスシャワー", 3],
+        ["[レッドストライフ]ゴールドシップ", 2],
+        ["[ワイルドトップギア]ウオッカ", 2],
+        ["[トップ・オブ・ブルー]ダイワスカーレット", 2],
+        ["[石穿つ者]グラスワンダー", 2],
+        ["[エル☆Número１]エルコンドルパサー", 2],
+        ["[エンプレスロード]エアグルーヴ", 2],
+        ["[すくらんぶる☆ゾーン]マヤノトップガン", 2],
+        ["[マーマリングストリーム]スーパークリーク", 2],
+        ["[ストレート・ライン]メジロライアン", 1],
+        ["[tach-nology]アグネスタキオン", 1],
+        ["[Go To Winning]ウイニングチケット", 1],
+        ["[サクラ、すすめ！]サクラバクシンオー", 1],
+        ["[うららん一等賞♪]ハルウララ", 1],
+        ["[運気上昇☆幸福万来]マチカネフクキタル", 1],
+        ["[ポインセチア・リボン]ナイスネイチャ", 1],
+        ["[キング・オブ・エメラルド]キングヘイロー", 1],
+        ["[オー・ソレ・スーオ！]テイエムオペラオー", 3],
+        ["[MB-19890425]ミホノブルボン", 3],
+        ["[pf.Victory formula...]ビワハヤヒデ", 3],
+        ["[ビヨンド・ザ・ホライズン]トウカイテイオー", 3],
+        ["[エンド・オブ・スカイ]メジロマックイーン", 3],
+        ["[フィーユ・エクレール]カレンチャン", 3],
+        ["[Nevertheless]ナリタタイシン", 3],
+        ["[あぶそりゅーと☆LOVE]スマートファルコン", 3],
+        ["[Maverick]ナリタブライアン", 13],
+        ["[サンライト・ブーケ]マヤノトップガン", 3],
+        ["[クエルクス・キウィーリス]エアグルーヴ", 3],
+        ["[あおぐもサミング]セイウンスカイ", 13],
+        ["[アマゾネス・ラピス]ヒシアマゾン", 3],
+        ["[ククルカン・モンク]エルコンドルパサー", 3],
+        ["[セイントジェード・ヒーラー]グラスワンダー", 3],
+        ["[シューティングスタァ・ルヴュ]フジキセキ", 3],
+        ["[オーセンティック/1928]ゴールドシチー", 3],
+        ["[ほっぴん♪ビタミンハート]スペシャルウィーク", 3],
+        ["[ぶっとび☆さまーナイト]マルゼンスキー", 3],
+        ["[ブルー/レイジング]メイショウドトウ", 3],
+        ["[Meisterscaft]エイシンフラッシュ", 3],
+        ["[吉兆・初あらし]マチカネフクキタル", 3],
+        ["[ボーノ☆アラモーダ]ヒシアケボノ", 3],
+        ["[超特急！フルカラー特殊PP]アグネスデジタル", 3],
+        ["[Make up Vampire!]ライスシャワー", 3],
+        ["[シフォンリボンマミー]スーパークリーク", 3],
+        ["[プリンセス・オブ・ピンク]カワカミプリンセス", 3],
+        ["[Creeping Black]マンハッタンカフェ", 3],
+        ["[皓月の弓取り]シンボリルドルフ", 3],
+        ["[秋桜ダンツァトリーチェ]ゴールドシチー", 3],
+        ["[ポップス☆ジョーカー]トーセンジョーダン", 3],
+        ["[ツイステッド・ライン]メジロドーベル", 3],
+        ["[キセキの白星]オグリキャップ", 3],
+        ["[ノエルージュ・キャロル]ビワハヤヒデ", 3],
+        ["[Noble Seamair]ファインモーション", 3],
+        ["[疾風迅雷]タマモクロス", 3],
+        ["[初うらら♪さくさくら]ハルウララ", 3],
+        ["[初晴・青き絢爛]テイエムオペラオー", 3],
+        ["[日下開山・花あかり]サクラチヨノオー", 3],
+        ["[CODE：グラサージュ]ミホノブルボン", 3],
+        ["[コレクト・ショコラティエ]エイシンフラッシュ", 3],
+        ["[クリノクロア・ライン]メジロアルダン", 3],
+        ["[Starry Nocturne]アドマイヤベガ", 3],
+        ["[錦上・大判御輿]キタサンブラック", 3],
+        ["[ぱんぱかティルトット]マチカネタンホイザ", 2],
+        ["[Natural Brilliance]サトノダイヤモンド", 3],
+        ["[ブリュニサージュ・ライン]メジロブライト", 3],
+        ["[ソワレ・ド・シャトン]セイウンスカイ", 3],
+        ["[シュクセ・エトワーレ]フジキセキ", 3],
+        ["[ティアード・ペタル]ニシノフラワー", 3],
+        ["[四白流星の襲]ヤエノムテキ", 3],
+        ["[RUN&WIN]ナイスネイチャ", 3],
+        ["[白く気高き激励の装]キングヘイロー", 3],
+        ["[オールタイム・フィーバー]アイネスフウジン", 3],
+        ["[Like Breakthrough]メジロパーマー", 3],
+        ["[朔月のマ・シェリ]カレンチャン", 3],
+        ["[Titania]ファインモーション", 3],
+        ["[稲荷所縁江戸紫]イナリワン", 3],
+        ["[プラタナス・ウィッチ]スイープトウショウ", 3],
+        ["[Bubblegum☆Memories]タイキシャトル", 3],
+        ["[バカンス・サフィール]メジロドーベル", 3],
+        ["[unsigned]エアシャカール", 3]
+    ]
+
+    # 確率比[★1, ★2, ★3, ピックアップ]
+    weights = [79, 18, 1.5, 1.5]
+    # 確率比(10回目)
+    weights_10 = [0, 97, 1.5, 1.5]
+
+    # 画像サイズ
+    width = 400
+    height = 222
+    # 項目の間隔
+    margin = 45
+    # 画像の背景色
+    bg = (54, 57, 63)
+    # 画像の初期化
+    img = Image.new("RGB", (width, height), bg)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(".fonts/meiryo.ttf", 16)
+
+    for i in range(10):
+        w = weights if i < 9 else weights_10
+
+        # レア度ごとに選出
+        uma_gacha_results_by_rarity = [
+            random.choice(list(filter(lambda e: e[1] == 1, uma_gacha_lists))),
+            random.choice(list(filter(lambda e: e[1] == 2, uma_gacha_lists))),
+            random.choice(list(filter(lambda e: e[1] == 3, uma_gacha_lists))),
+            random.choice(list(filter(lambda e: e[1] > 10, uma_gacha_lists)))
+        ]
+
+        # 最終的な排出ウマ娘を決定
+        uma_gacha_result = random.choices(uma_gacha_results_by_rarity, weights=w)[0]
+
+        # レア度が3なら文字色を変える
+        color = (214, 204, 107) if uma_gacha_result[1] % 10 == 3 else (255, 255, 255)
+
+        # 原寸で表示される最大の画像サイズが400x300(10連だと見切れる)なので5連ずつ2枚の画像に分ける
+        if i % 5 == 0:
+            draw.rectangle((0, 0, width, height), fill=bg)
+
+        # アイコン画像をuma_iconフォルダから読み込み&貼り付け(URLから読み込むと遅かった)
+        uma_image = Image.open(f"resources/uma_icon/i_{uma_gacha_lists.index(uma_gacha_result) + 1}.png")
+        img.paste(uma_image, (3, margin * (i % 5) + 5))
+
+        # テキストを描画(星マーク)
+        draw.text((40, margin * (i % 5)), "★" * (uma_gacha_result[1] % 10), color, font=font)
+        # テキストを描画(ウマ娘名称)
+        draw.text((40, margin * (i % 5) + 15), uma_gacha_result[0], color, font=font)
+
+        # 5連ごとに画像を書き出し
+        if i % 5 == 4:
+            img.save(f"resources/temporally/uma_gacha_{ctx.channel.id}_{int(i / 5) + 1}.png")
+
+    glob_uma_gacha_result_images = glob.glob(f"resources/temporally/uma_gacha_{ctx.channel.id}_*.png")
+
+    uma_gacha_result_images = list(map(lambda e: discord.File(e), glob_uma_gacha_result_images))
+    await ctx.channel.send(files=uma_gacha_result_images)
+
+    for file in glob_uma_gacha_result_images:
+        if os.path.isfile(file):
+            os.remove(file)
 
 
 
