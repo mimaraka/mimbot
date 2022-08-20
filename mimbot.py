25ddb75bdaed64cf55d29a9b07f4015cc3960b7c
mimaraka <nj.ot.niconico@gmail.com>
2022-08-20 15:16:06 +0900

    add temp

--- mimbot.py
+++ mimbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 #import cv2
 #herokuでcv2をimportするとエラーが出たのでとりあえずPillowで代用
 import discord
+import datetime
 import glob
 from PIL import Image, ImageFont, ImageDraw
 #import librosa
